0c5d7b1c660bb432710e98f425e843bac3932076
Jonasjoja <jona73c4@stud.kea.dk>
2020-08-28 09:28:08 +0200

    Initial Commit

---
+++ ContactInfoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ContactInfoApp
+//
+//  Created by admin on 28/08/2020.
+//  Copyright © 2020 Signe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
